801e3b28dbe3ff8d0da68b1529ce840171f16b99
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-31 14:30:15 -0700

    whack a mole and 2 Evil songs.

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,13 +1,23 @@
 package _11_whack_a_mole;
 
+import java.applet.AudioClip;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 	
+	Date timeNow = new Date();
+	int molesGot = 0;
+	int moleMiss = 0;
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	
@@ -17,11 +27,63 @@ public class WhackAMole {
 	}
 	
 	public void go() {
-		panel.setLayout(new GridLayout(4,6));
+		Random rand = new Random();
+		int mole = rand.nextInt(24);
+		panel.setLayout(new GridLayout(4, 6, 5, 5));
 		for (int i = 0; i < 24; i++) {
-			panel.add(new JButton());
+			JButton button = new JButton();
+			panel.add(button);
+			if (i == mole) {
+				button.setText("X");
+			}
+			button.addActionListener(this);
 		}
+		frame.add(panel);
 		frame.setVisible(true);
-		frame.pack();
+		frame.setSize(307, 211);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	void speak(String words) {
+	     try {
+	          Runtime.getRuntime().exec("say " + words).waitFor();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}
+	
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		frame.remove(panel);
+		frame.dispose();
+		panel = new JPanel();
+		if (((JButton) arg0.getSource()).getText().equals("")) {
+			playSound("wrong song.wav");
+		} else {
+			playSound("mole song.wav");
+		}
+		try {
+			Thread.sleep(9000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+		if (molesGot + moleMiss >= 10) {
+			endGame(timeNow, molesGot);
+		} else {
+			go();
+		}
 	}
 }
